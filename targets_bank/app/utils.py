72a23678c7fa387a4e98a2ffa3f6f130a934ec43
kobi <kodigershuni@gmail.com>
2026-03-16 14:26:19 +0200

    updated tables fields

--- targets_bank/app/utils.py
+++ targets_bank/app/utils.py
@@ -2,8 +2,10 @@ entities_table_query = """
 CREATE TABLE IF NOT EXISTS entities
 (
     id VARCHAR(20) PRIMARY KEY,
-    name VARCHAR(40),
-    type VARCHAR(40),
+    reported_lat DECIMAL(20, 20),
+    reported_lon DECIMAL(20, 20),
+    priority_level INT,
+    distance DECIMAL(20, 20),
     status VARCHAR(30)
 )
 """
@@ -13,10 +15,10 @@ CREATE TABLE IF NOT EXISTS intel
     signal_id VARCHAR(100) PRIMARY KEY,
     timestamp DATE,
     entity_id VARCHAR(20),
-    reported_lat INT,
-    reported_lon INT,
+    reported_lat DECIMAL(20, 20),
+    reported_lon DECIMAL(20, 20),
     signal_type VARCHAR(20),
-    priority_level int
+    priority_level INT
 )
 """
 
