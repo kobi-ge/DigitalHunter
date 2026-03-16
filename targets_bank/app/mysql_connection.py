e681c0c5ed8ba079ae76a7545107db370608ee44
kobi <kodigershuni@gmail.com>
2026-03-16 11:30:56 +0200

    established connection to mysql

---
+++ targets_bank/app/mysql_connection.py
@@ -0,0 +1,26 @@
+from mysql.connector import connect, errors
+import os
+
+class MysqlConnection:
+    def __init__(self, host, port, password, user, database, logger):
+        self.host = host
+        self.port = port
+        self.password = password
+        self.user = user
+        self.database = database
+        self.logger = logger
+
+    def connect(self):
+        try:
+            self.con = connect(
+                host = self.host,
+                port = self.port,
+                password = self.password,
+                user = self.user,
+                database = self.database
+            )
+            self.cursor = self.con.cursor()
+            self.logger.info(f"connection with mysql established")
+        except errors.Error as e:
+            self.logger.error(f"error connecting to mysql: {e}")
+
